77183d6d886356709dd965e98b043342b06ac517
YevhenChu <inflash80@gmail.com>
2024-09-11 14:05:16 +0200

    Fixed a simple test

--- app/tests/ExampleTest.php
+++ app/tests/ExampleTest.php
@@ -6,7 +6,7 @@ namespace App\Tests;
 
 use PHPUnit\Framework\TestCase;
 
-class ExampleTest extends TestCase
+final class ExampleTest extends TestCase
 {
     public function test_basic_test() : void
     {
